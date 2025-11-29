b3472377c69816577ecac698d64127df9a4624fc
Nourishere <nournawar5@gmail.com>
2025-11-29 19:32:53 +0200

    Added implementation for backtrace() and added it as a log when the kernel panics

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,6 +121,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -131,4 +132,15 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void 
+backtrace(void) {
+    uint64 fp = r_fp();
+    while((PGROUNDDOWN(fp) - PGROUNDUP(fp)) > 0) {
+        // return address is at fp - 8
+        printf("%p\n",*((uint64*)(fp - 8)));
+		// previous fp is at fp - 16
+		fp = *((uint64*) (fp - 16));
+    }
 }
