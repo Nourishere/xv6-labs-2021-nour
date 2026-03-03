32cd6cb135544b928ea41e5f4dcbeb9545413af6
Nour Nawar <nerdyforsciences@gmail.com>
2026-03-04 01:57:17 +0200

    Implement  system call

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -513,3 +513,34 @@ sys_pipe(void)
   }
   return 0;
 }
+
+// Create a new symlink
+uint64
+sys_symlink(void)
+{
+  struct inode *ip;
+  char target[MAXPATH], path[MAXPATH];
+
+  if(argstr(0, target, MAXPATH) < 0)
+	return -1;
+  if(argstr(1, path, MAXPATH) < 0)
+	return -1;
+
+  begin_op();
+
+  // create inode and mark it as T_SYMLINK
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+	end_op();
+	return -1;
+  }
+  // store target path in the inode data field
+  if(writei(ip, 0, (uint64) target, 0, strlen(target) ) < strlen(target)){
+	iunlockput(ip);
+	end_op();
+	return -1;
+  }
+  iunlockput(ip);
+  end_op();
+  return 0;
+}
