476a4b0a8c324b118fd926f61be22eadc207f1c0
Nour Nawar <nerdyforsciences@gmail.com>
2026-03-14 13:10:45 +0200

    Lab mmap: add min(a,b) macro
    I tend to use this in several places.

--- kernel/defs.h
+++ kernel/defs.h
@@ -186,3 +186,6 @@ void            virtio_disk_intr(void);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
+
+// Helpful macros
+#define min(a, b) ((a) < (b) ? (a) : (b))
